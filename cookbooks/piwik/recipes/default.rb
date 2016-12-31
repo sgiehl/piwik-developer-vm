2e8fc900f76479be3b8fbdd614a9e13be40fe57d
Marc Neudert <marc.neudert@gmail.com>
2016-12-31 16:58:37 +0100

    runs composer install/update

--- cookbooks/piwik/recipes/default.rb
+++ cookbooks/piwik/recipes/default.rb
@@ -1,8 +1,23 @@
-packages = %w(mysql-server php5 php5-mysql)
+packages = %w(mysql-server php5 php5-curl php5-mysql)
 
 packages.each do |pkg|
   apt_package pkg do
   end
 end
 
+
+composer_action = :install
+piwik_source    = '/srv/piwik'
+piwik_vendor    = "#{piwik_source}/vendor"
+
+if File.directory?(piwik_vendor)
+  composer_action = :update
+end
+
 include_recipe 'composer::self_update'
+
+composer_project piwik_source do
+  dev    true
+  quiet  true
+  action composer_action
+end
