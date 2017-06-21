adbf6ccfc2c8f3862c8dcc8d1c944a21b816d41b
sgiehl <stefan@piwik.org>
2017-06-21 22:11:22 +0200

    increase maximum nesting level as UI tests otherwise will fail with xdebug enabled

--- cookbooks/piwik/attributes/default.rb
+++ cookbooks/piwik/attributes/default.rb
@@ -11,3 +11,5 @@ default['piwik']['type']            = config.type
 default['redisio']['bin_path']        = '/usr/bin'
 default['redisio']['package_install'] = true
 default['redisio']['version']         = nil
+
+default['php']['directives'] = {'xdebug.max_nesting_level': 200}
