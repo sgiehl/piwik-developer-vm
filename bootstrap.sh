93c3e53f8a4bc94bf4ec06a2259f96ee8619d0a3
sgiehl <stefan@matomo.org>
2021-06-28 13:59:24 +0200

    use chef workstation instead of deprecated chef dk

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-CHEFDK_PKG='chefdk_4.12.0-1_amd64.deb'
-CHEFDK_SHA256='04f4a52eb287f076c7be3539d975e665dce686714c097a6716a85d4e6eb65e8e'
+CHEFDK_PKG='chef-workstation_21.6.497-1_amd64.deb'
+CHEFDK_SHA256='741a6efc6b83ad789ffa8411f860552fb2211c7f6d6ccd983fe4e0008329a780'
 
 command -v berks >/dev/null 2>&1 || {
   echo 'Updating APT repositories...'
@@ -10,14 +10,14 @@ command -v berks >/dev/null 2>&1 || {
   cd /tmp
 
   [ ! -f "${CHEFDK_PKG}" ] && {
-    echo 'Downloading Chef Development Kit...'
+    echo 'Downloading Chef Workstation...'
 
-    wget --quiet "https://packages.chef.io/files/stable/chefdk/4.12.0/ubuntu/18.04/${CHEFDK_PKG}"
+    wget --quiet "https://packages.chef.io/files/stable/chef-workstation/21.6.497/ubuntu/18.04/${CHEFDK_PKG}"
 
     dl_sha256="$(sha256sum /tmp/${CHEFDK_PKG} | awk '{ print $1 }')"
 
     if [ "${CHEFDK_SHA256}" != "${dl_sha256}" ]; then
-      echo 'Chef Development Kit download checksum mismatch!'
+      echo 'Chef Workstation download checksum mismatch!'
       echo "Expected: ${CHEFDK_SHA256}"
       echo "Received: ${dl_sha256}"
 
